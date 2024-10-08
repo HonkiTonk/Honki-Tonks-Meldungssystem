aafffcf2b18c62a0246b785c6a875de5ddd584ce
HonkiTonk <totalplanlos.de@web.de>
2024-10-08 23:03:04 +0200

    Ordnerstruktur überarbeitet.

--- HonkiTonksMeldungssystem/src/Changelog.ads
+++ HonkiTonksMeldungssystem/src/Changelog.ads
@@ -1,9 +1,10 @@
 package Changelog is
    
-   -- Version 1.00.0000 -> 1.02.0000 (08.10.2024):
+   -- Version 1.00.0000 -> 1.03.0000 (08.10.2024):
    
    -- Englische Version hinzugefügt.
    -- Interne Benennungen überarbeitet.
+   -- Ordnerstruktur überarbeitet.
    
    
 
